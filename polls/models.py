a9b6d98dd14a7538b7aa68bf1762cbc487457b28
[techsynt] <[techsynt@gmail.com]>
2020-10-13 16:52:19 +0300

    changed models

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.utils import timezone
+import datetime
+
 
 # Create your models here.
-class question(models.Model):
+class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date_published')
-class choice(models.Model):
-    question = models.ForeignKey(question, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
